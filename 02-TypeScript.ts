fbf61f26497cbe91a877100aa9bdb415c60e9cfb
Rati Elizbarashvili <elizbarashvilirati15@Gmail.com>
2025-06-30 04:16:37 +0400

    types done

--- 02-TypeScript.ts
+++ 02-TypeScript.ts
@@ -2,21 +2,25 @@
 
 // მაგალითი 1
 
-let name = "Alice";
+let name1 : String = "Alice";
 
 // მაგალითი 2
 
-let numbers = [1, 2, 3];
+let numbers : Number[] = [1, 2, 3];
 
 // მაგალითი 3
 
-function multiply(a, b) {
+function multiply( a : number, b : number): number {
   return a * b;
 }
 
 // მაგალითი 4
+type User2 = {
+  id: number;
+  name: string;
+};
 
-const user = { id: 1, name: "Alice" };
+const user2: User2 = { id: 1, name: "Alice" };
 
 // მაგალითი 5
 // რისთვის ვიყენებთ არსებული კოდში = Guest-ს :
@@ -28,7 +32,18 @@ function greet(name: string = "Guest") {}
 // მაგალითი 6
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type Config = {
 
-const config1 = {
+type Config = {
+  theme? : string;
+  options?: {
+    fontSize?: number;
+    layout?: string | null;
+  };
+  tester?: string;
+}
+
+
+
+const config1 : Config = {
   theme: "dark",
   options: {
     fontSize: 16,
@@ -36,19 +51,26 @@ const config1 = {
   },
 };
 
-const config2 = {
+const config2 : Config = {
   theme: "dark",
 };
 
-const config3 = {
+const config3 : Config = {
   tester: "test",
 };
 
+
+
+
 // მაგალითი 7
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type MixedArray =
-const array1 = [42, "hello", { name: "Alice" }];
-const array2 = ["apple", true, { isValid: false }];
-const array3 = [];
+
+type MixedArray = (number | string | boolean | { [key: string]: any })[];
+
+
+const array1 : MixedArray = [42, "hello", { name: "Alice" }];
+const array2 : MixedArray = ["apple", true, { isValid: false }];
+const array3 : MixedArray = [];
 
 // მაგალითი 8
 // აღწერეთ რისი ტიპიზაცია ხდება არსებულ კოდში წერილობით
@@ -59,11 +81,32 @@ type Handler = {
   log?: () => void;
 };
 
+//  process -  ფუნქცა რომელიც არგუმენტს არ ირებს და აბრუნებს სტრინგს
+//  validate - ფუნქცი რომელიც იღებს სტრინგს და აბრუნებს ბულეანს
+//  log -  აბრუნებს ვოიდს
+
 // მაგალითი 9
 
 // შექმენით ტიპი მონაცემისთივს:
 
-type User = {};
+type User = {
+  id: number;
+  username: string;
+  isAdmin: boolean;
+  profile: {
+    fullName: string;
+    age: number;
+    interests: string[];
+  };
+  settings: {
+    theme: string;
+    notifications: {
+      email: boolean;
+      sms: boolean;
+    };
+  };
+  metadata?: any; 
+};
 
 const user: User = {
   id: 101,
